f3d0d7f56412c338332687b597b2c560bb234414
DaniMiki97 <n.d.1997.zelo@hotmail.com>
2016-05-25 22:07:38 -0500

    Variable

--- JumpHydraulic/salto.h
+++ JumpHydraulic/salto.h
@@ -1,3 +1,5 @@
+//funciones
+
 #ifndef SALTO_H
 
 #define SALTO_H
@@ -8,9 +10,8 @@
 class Salto
 {
 public:
-    F V1,V2,Y1,Y2,Q,F1,F2;
-    CF G;
-    Salto();
+    F V1,V2,Y1,Y2,Q,F1,F2,E1,E2,Ta;
+    Salto(F,F,F);
     F Discharge (F, F);
     F Energy_Specific_Before(F,F);
     F Energy_Specific_After(F,F);
@@ -19,8 +20,7 @@ public:
     F Number_Froude(F,F); // entradas: critical_depth and critical_velocity
 
 
-    virtual ~Salto();
+    //virtual ~Salto();
 };
 
 #endif // SALTO_H
-//Pruebbaaaaa
