dc0ccc307ca3c1bdf67ff29b427a2acc8692e0c7
DaniMiki97 <n.d.1997.zelo@hotmail.com>
2016-05-25 22:08:45 -0500

    Variable

--- JumpHydraulic/tipos.h
+++ JumpHydraulic/tipos.h
@@ -1,13 +1,13 @@
+//Renombrado de tipos
+
 #ifndef TIPOS
 #define TIPOS
 
 typedef int Ent;
 typedef double Real;
 typedef float F;
-typedef const float CF;
 typedef const int CEnt;
-// holaaaaaa
-//holaaa
+const F G=9.78;
+
 
 #endif // TIPOS
-
